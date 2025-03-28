a428a70648b80fca9676ab75abd2b50fd96e3944
Pruden <prudencosta@gmail.com>
2025-03-28 17:30:41 +0100

    post para modos de juego

--- src/Controller/ModoDeJuegoController.php
+++ src/Controller/ModoDeJuegoController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Jugador;
+use App\Entity\ModosJuego;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Routing\Annotation\Route;;
 
 
 class ModoDeJuegoController extends AbstractController
@@ -15,18 +20,64 @@ class ModoDeJuegoController extends AbstractController
 
 
     /**
-     * @Route("/clan/{id}/unirse/{idJugador}",methods={"POST"})
+     * @Route("/crearModoJuego",methods={"POST"})
      */
-    public function jugadorSeUneAUnClan(Request $request, EntityManagerInterface $entityManager)
-    {
+    public function crearModoJuego(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): Response {
+        $modoJuegoJson = $request->request->get('modoJuego');
 
+        if (!$modoJuegoJson) {
+            return new JsonResponse(["error" => "No se recibió el objeto modoJuego"], 400);
+        }
 
+        $data = json_decode($modoJuegoJson, true);
 
+        if (!isset($data['idJugador'])) {
+            return new JsonResponse(["error" => "idJugador no proporcionado"], 400);
+        }
 
+        $jugador = $entityManager->getRepository(Jugador::class)->find($data['idJugador']);
 
+        if (!$jugador) {
+            return new JsonResponse(["error" => "Jugador no encontrado"], 404);
+        }
 
+        unset($data['idJugador']);
+        $modoJuegoJsonSinJugador = json_encode($data);
+        $modoJuego = $serializer->deserialize($modoJuegoJsonSinJugador, ModosJuego::class, 'json');
 
-        return new JsonResponse("Ok",Response::HTTP_OK);
+        $modoJuego->setJugador($jugador);
+
+        $file = $request->files->get('imagen');
+        if ($file instanceof UploadedFile) {
+            $directorioBase = $this->getParameter('uploads_directory');
+            $nombreArchivo = uniqid() . '.' . $file->guessExtension();
+            $directorioDestino = $directorioBase . '/modosDeJuego';
+            try {
+                $file->move($directorioDestino, $nombreArchivo);
+                $modoJuego->setImagen('/uploads/modosDeJuego/' . $nombreArchivo);
+            } catch (FileException $e) {
+                return new JsonResponse(["error" => "Error al subir la imagen"], 500);
+            }
+        }
+
+        $ultimoModo = $entityManager->getRepository(ModosJuego::class)
+            ->createQueryBuilder('m')
+            ->select('MAX(m.idnummodo)')
+            ->where('m.jugador = :jugador')
+            ->setParameter('jugador', $jugador)
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        $nuevoIdNumModo = ($ultimoModo ?? 0) + 1;
+        $modoJuego->setIdnummodo($nuevoIdNumModo);
+
+        $entityManager->persist($modoJuego);
+        $entityManager->flush();
+
+        return new JsonResponse([
+            "mensaje" => "Modo de juego creado correctamente",
+            "rutaImagen" => $modoJuego->getImagen()
+        ], 201);
     }
 
 
