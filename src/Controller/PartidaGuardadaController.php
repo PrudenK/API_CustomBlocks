789c3c681a2f345166cb19c3097b7deeb33e58cc
Pruden <prudencosta@gmail.com>
2025-03-31 13:06:08 +0200

    POST subir partida guardada

--- src/Controller/PartidaGuardadaController.php
+++ src/Controller/PartidaGuardadaController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\DTO\PartidaGuardadaDTO;
 use App\Entity\Clan;
 use App\Entity\Jugador;
 use App\Entity\MensajeClan;
+use App\Entity\PartidaGuardada;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
@@ -18,5 +20,58 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class PartidaGuardadaController extends AbstractController
 {
+    /**
+     * @Route("/guardarPartida",methods={"POST"})
+     */
+    public function guardar(Request $request, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse
+    {
+        try {
+            $data = $request->getContent();
 
+            /** @var PartidaGuardadaDTO $dto */
+            $dto = $serializer->deserialize($data, PartidaGuardadaDTO::class, 'json');
+
+            $jugador = $em->getRepository(Jugador::class)->findOneBy(["id" => $dto->idJugador]);
+
+            if (!$jugador) {
+                return new JsonResponse(['error' => 'Jugador no encontrado'], 404);
+            }
+
+            $partida = new PartidaGuardada();
+            $partida->setIdJugador($jugador);
+            $partida->setNumPartidaGuardada($dto->numPartidaGuardada);
+            $partida->setModo($dto->modo);
+            $partida->setTiempo($dto->tiempo);
+            $partida->setPuntuacion($dto->puntuacion);
+            $partida->setLineas($dto->lineas);
+            $partida->setNivel($dto->nivel);
+            $partida->setTableroPartida($dto->tableroPartida);
+            $partida->setTamaTablero($dto->tamaTablero);
+            $partida->setDiseTablero($dto->diseTablero);
+            $partida->setDiseTableroSecun($dto->diseTableroSecun);
+            $partida->setSiguientesPiezasActivo($dto->siguientesPiezasActivo);
+            $partida->setSiguientesPiezas($dto->siguientesPiezas);
+            $partida->setArrayPiezas($dto->arrayPiezas);
+            $partida->setDisePiezas($dto->disePiezas);
+            $partida->setHoldActivo($dto->holdActivo);
+            $partida->setDashActivo($dto->dashActivo);
+            $partida->setVelocidadCaidaActual($dto->velocidadCaidaActual);
+            $partida->setLineasParaSaltoDeNivel($dto->lineasParaSaltoDeNivel);
+            $partida->setSaltoDeTiempoPorNivel($dto->saltoDeTiempoPorNivel);
+            $partida->setLimiteRotaciones($dto->limiteRotaciones);
+            $partida->setPiezaActual($dto->piezaActual);
+            $partida->setPosicionPiezaActual($dto->posicionPiezaActual);
+            $partida->setNumRotacionesDeLaPiezaActual($dto->numRotacionesDeLaPiezaActual);
+
+            $em->persist($partida);
+            $em->flush();
+
+            return new JsonResponse(['success' => true, 'id' => $partida->getId()], 201);
+        } catch (\Exception $e) {
+            return new JsonResponse([
+                'success' => false,
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
 }
---
+++ src/DTO/PartidaGuardadaDTO.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DTO;
+
+class PartidaGuardadaDTO
+{
+    public int $idJugador;
+    public int $numPartidaGuardada;
+    public string $modo;
+    public string $tiempo;
+    public int $puntuacion;
+    public int $lineas;
+    public int $nivel;
+    public array $tableroPartida;
+    public int $tamaTablero;
+    public int $diseTablero;
+    public int $diseTableroSecun;
+    public int $siguientesPiezasActivo;
+    public string $siguientesPiezas;
+    public string $arrayPiezas;
+    public int $disePiezas;
+    public int $holdActivo;
+    public int $dashActivo;
+    public int $velocidadCaidaActual;
+    public int $lineasParaSaltoDeNivel;
+    public int $saltoDeTiempoPorNivel;
+    public int $limiteRotaciones;
+    public string $piezaActual;
+    public string $posicionPiezaActual;
+    public int $numRotacionesDeLaPiezaActual;
+}
